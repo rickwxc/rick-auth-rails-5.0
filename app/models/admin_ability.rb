1e32487735f2a0d7095ab970c45ac106276b4124
rick <lurker2000wxc@gmail.com>
2016-07-25 04:15:35 +0000

    rails admin with cancancan

---
+++ app/models/admin_ability.rb
@@ -0,0 +1,9 @@
+class AdminAbility
+  include CanCan::Ability
+  def initialize(user)
+    if user && user.admin?
+      can :access, :rails_admin
+      can :manage, :all   
+    end
+  end
+end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -14,7 +14,7 @@ RailsAdmin.config do |config|
 	config.current_user_method(&:current_user)
 
 	## == Cancan ==
-	# config.authorize_with :cancan
+	config.authorize_with :cancan, AdminAbility
 
 	## == Pundit ==
 	# config.authorize_with :pundit
