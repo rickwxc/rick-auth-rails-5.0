11f86fe1e9bcc3ab4683692f1fd1e699d7a3e17f
rick <lurker2000wxc@gmail.com>
2016-11-30 11:33:59 +1100

    self.ar_save_load

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,30 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.ar_save_load(data, load_keys)
+	  kv = {}
+
+	  if load_keys
+		  load_keys.each do |k|
+			  kv[k] = data[k]
+		  end
+	  else
+		  kv = data
+	  end
+
+	  obj = self.find_or_initialize_by(kv)
+
+	  if load_keys
+		  data.each do |k,v|
+			  if obj.has_attribute?(k) 
+				  obj[k] = v
+			  end
+		  end
+	  end
+
+	  obj.save
+
+	  obj
+  end
+
 end
