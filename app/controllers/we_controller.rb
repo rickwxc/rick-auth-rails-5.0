47568f9aef39a4dd2611a15824d90eeb58bdf2e2
rick <lurker2000wxc@gmail.com>
2016-12-09 12:08:03 +1100

    login with url redirect

--- app/controllers/we_controller.rb
+++ app/controllers/we_controller.rb
@@ -8,11 +8,18 @@ class WeController < ApplicationController
     def login
         id = params[:user_id]
         code = params[:code]
+        url = params[:url]
         user = User.where(:id => id).first
 
         dbcode = user.login_code
         if code == dbcode
-            sign_in_and_redirect user
+			if url
+				sign_in user, event: :authentication 
+				url = Base64.urlsafe_decode64(url)
+				redirect_to url
+			else
+				sign_in_and_redirect user
+			end
         else
             redirect_to new_user_session_path
         end
