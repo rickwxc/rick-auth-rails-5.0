de9a5fdcfa9a640154542e1f2f67b5335bd86370
rick <lurker2000wxc@gmail.com>
2017-02-21 16:53:37 +1100

    bundle

--- app/models/auth_bundle.rb
+++ app/models/auth_bundle.rb
@@ -1,3 +1,23 @@
 class AuthBundle < ApplicationRecord
 	belongs_to:auth_bundle_type
+	has_many :auth_bundle2obj
+
+
+	def objs
+		list = []
+		self.auth_bundle2obj.each do |v|
+			t = {}
+
+			obj = v['auth_obj_model_name'].constantize.where(:id => v.auth_obj_id).first
+			if obj
+				t[:bundleobj] = v
+				t[:orgobj] = obj
+				list << t
+			end
+		end
+
+
+		list
+	end
+
 end
